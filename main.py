695e25d24350ab7f3b82565ebbd947548f004d14
KebbaPhilip <kebbaphilip@gmail.com>
2026-09-15 21:58:53 +0000

    create pixela account

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import os
+from dotenv import load_dotenv
+import requests
+
 def main():
-    print("Hello from habit-tracker!")
+    load_dotenv()
+    pixela_endpoint = "https://pixe.la/v1/users"
+    token = os.getenv("PIXELA_TOKEN")
+
+    parameters = {
+        "token":token,
+        "username": "bee107",
+        "agreeTermsOfService": "yes",
+        "notMinor": "yes"
+
+    }
+
+    #create account
+    # response = requests.post(url=pixela_endpoint, json=parameters)
+    # print(response.text)
 
 
 if __name__ == "__main__":
